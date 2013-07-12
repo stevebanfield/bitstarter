93c45dabf6fa38d502dc0df60cb7f17a1b78e622
Steve Banfield <sbanfield@gmail.com>
2013-07-11 18:56:22 -0700

    updated web.jsp

--- web.js
+++ web.js
@@ -1,6 +1,26 @@
+// new code
+/*
+require('buffer');
+
+var sourceName = "index.html";
+var contents = "null";
+var fs = require('fs');
+
+contents = fs.readSync(sourceName, "utf-8");
+*/
+
+require('fs').readFileSync('file.txt').toString().split(/\r?\n/).forEach(function(line){
+  console.log(line);
+})
+
+
+/* original web.js code
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
+
+
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
@@ -10,3 +30,5 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+*/
