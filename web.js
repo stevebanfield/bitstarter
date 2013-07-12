0757bb4c399268e178dba7b23845a37a1897f938
Steve Banfield <sbanfield@gmail.com>
2013-07-11 22:05:05 -0700

    ugh

--- web.js
+++ web.js
@@ -1,38 +1,29 @@
 // new code
 
 var express = require('express');
-
 var fs = require('fs');
-var sourceName = "index.html";
-var size = 28;
+var sourceName = 'index.html';
+var size = 50;
 
-// var contents = new Buffer(size);
+var contents = new Buffer(size);
 
-var contents = fs.readFileSync(sourceName, 'utf-8');
-
-console.log(contents);
+contents = fs.readFileSync(sourceName);
+console.log(contents.toString('utf-8'));
 
 // var app = express.createServer(express.logger());
 
 // app.get('/', function(request, response) {
-// //  response.send(contents.toString('utf-8'));
-//     response.send(contents);
-
+//   response.send(contents.toString('utf-8'));
+// //    console.log(contents.toString('utf-8'));
 // });
 
-// console.log(contents.toString('utf-8'));
-
 // var port = process.env.PORT || 5000;
-// app.listen(port);
+// app.listen(port, function() {
+//   console.log("Listening on " + port);
+// });
 
 
-// response.send(contents.toString('utf-8'));
 
-/* require('fs').readFileSync(sourceName).toString().split(/\r?\n/).forEach(function(line){
-  console.log(line);
-//    response.send(line);
-});
-*/
 
 /* original web.js code
 
